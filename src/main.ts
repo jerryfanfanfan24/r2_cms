046655f455833df14934f525a25256a0fa427fc0
jf <peng07613@gmail.com>
2025-08-28 21:52:47 +0800

    不符合eslint的提交

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,13 @@ import pinia from './stores'
 import registerIcons from './global/register-icons'
 
 const app = createApp(App)
+
+
+
 app.use(registerIcons)
 app.use(pinia)
-app.use(router)
+app.use(router);
+
+
+
 app.mount('#app')
